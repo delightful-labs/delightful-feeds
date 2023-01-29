f60b598d862ed955627e3e30bf0fcad26021767a
mjamesderocher <mjamesderocher@gmail.com>
2023-01-28 21:23:36 -0500

    Ported InstantDB to Svelte

--- src/lib/utils/instant-db-helpers.js
+++ src/lib/utils/instant-db-helpers.js
@@ -1,51 +1,120 @@
 import ReactiveDB, { optimisticStore } from "@instantdb/react/dist/ReactiveDB"
 import { assertUnreachable } from "@instantdb/react/dist/util/error"
-import { writable } from "svelte/store"
+
+import { assert } from "@instantdb/react/dist/util/error";
+import * as uuid from "uuid";
+//import * as r from "./react";
+import * as instatx from "@instantdb/react/dist/instatx"
+import * as instaql from "@instantdb/react/dist/instaql"
+import * as authAPI from "@instantdb/react/dist/auth"
 
 /**
+ * @typedef {import("@instantdb/react/dist/instatx").TransactionChunk} TransactionChunk
+ * @typedef {import("@instantdb/react/dist/ReactiveDB").default} ReactiveDB
  * @typedef {import("@instantdb/react/dist/ReactiveDB").Auth} Auth
  * @typedef {import("@instantdb/react/dist/ReactiveDB").InstaQL} InstaQL
  * @typedef {import("@instantdb/react/dist/ReactiveDB").QueryResponse} QueryResponse
  * @typedef {import("@instantdb/react/dist/ReactiveDB").State} State
- * @typedef {[boolean, Error|undefined, Auth|undefined]} InitState
  * @typedef {import("@instantdb/react/dist/ReactiveDB").Config} Config
- * @typedef {import("svelte/store").Writable<InitState>} WritableState
  */
 
+// ------
+// Global
+
 /**
- * @param {State} state
- * @returns {InitState}
+ * @type {ReactiveDB | null}
  */
-function initState(state) {
-  switch (state.status) {
-    case "not-started":
-    case "connecting":
-      return [true, undefined, undefined];
-    case "shutdown":
-    case "error":
-      return [false, new Error(state.message), undefined];
-    case "online":
-    case "offline":
-      return [false, undefined, state.auth];
-    default:
-      assertUnreachable(state);
-  }
+let _GLOBAL_DB = null;
+
+/**
+ * @returns {ReactiveDB | null}
+ */
+export function globalDBAsserting() {
+  assert(_GLOBAL_DB != null, "You must `init` first");
+  return _GLOBAL_DB;
 }
+
+// -----
+// React
+
+/**
+ * 
+ * @returns {string}
+ */
+export function id() {
+  return uuid.v4();
+}
+
+
+/**
+ * @param {InstaQL} q
+ */
+export function query(q) {
+  const db = globalDBAsserting();
+
+  if (db?.state.status !== "online" && db?.state.status !== "offline") {
+    return {};
+  }
+
+  return instaql.query(q, optimisticStore(db.state));
+}
+
+// export function useQuery(q: InstaQL) {
+//   return r.useQuery(q, globalDBAsserting());
+// }
 
 /**
  * @param {Config} config
- * @returns {WritableState}
+ * @returns {ReactiveDB}
  */
 export function useInit(config) {
-  const db = new ReactiveDB(config)
-  db.start();
-  //@todo: I think I need to learn how to make a custom store out of this.
-  const state = writable(db)
-  //const [state, setState] = useState(initState(db.state));
-  //   const unsub = db.subscribe((state) => {
-  //     setState(initState(state));
-  //   });
-  //   return unsub;
-  // }, []);
-  return state;
+  if (!_GLOBAL_DB) {
+    _GLOBAL_DB = new ReactiveDB(config);
+  }
+  _GLOBAL_DB.start()
+  _GLOBAL_DB.subscribe((state) => {
+    console.log(state)
+  })
+  return _GLOBAL_DB
+}
+
+// -----
+// Auth
+
+export const auth = {
+  //{ email: string }
+  sendMagicCode({ email }) {
+    const db = globalDBAsserting();
+    return authAPI.sendMagicCode({
+      apiURI: db.config.apiURI,
+      appId: db.config.appId,
+      email: email,
+    });
+  },
+  //{ email: string; code: string }
+  verifyMagicCode({ email, code }) {
+    const db = globalDBAsserting();
+    return authAPI.verifyMagicCode({
+      apiURI: db.config.apiURI,
+      appId: db.config.appId,
+      email,
+      code,
+    });
+  },
+  signOut() {
+    const db = globalDBAsserting();
+    authAPI.signOut({ appId: db.config.appId });
+  },
+};
+
+// --------
+// Transact
+
+export const tx = instatx.tx;
+
+/**
+ * @param {instatx.TransactionChunk | instatx.TransactionChunk[]} x
+ */
+export function transact(x) {
+  globalDBAsserting().pushTx(x);
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
-  import { useInit } from '../lib/utils/instant-db-helpers'
+  import { useInit, tx, transact } from '../lib/utils/instant-db-helpers'
+  import * as instaql from "@instantdb/react/src/instaql"
+  import { optimisticStore } from '@instantdb/react/dist/ReactiveDB'
+  import { onMount } from 'svelte'
 
   const APP_ID = '82caf185-a700-4755-873a-23fcc3d862cc'
 
@@ -9,8 +12,30 @@
     apiURI: 'https://api.instantdb.com',
   })
 
+  const query = {
+    counter: {
+      $: {
+        where: {
+          id: 'singleton',
+        },
+        cardinality: 'one',
+      },
+    },
+  }
+  //const { counter } = useQuery(query)
+  const count = 0//counter?.count || 0
+
   $: console.log($db)
+
+  $: if ($db) {
+    console.log(instaql.query(query, optimisticStore(db.state)))
+  }
+
+  $: if ($db?.status === 'online' && count === 0) {
+    //transact([tx.counter['singleton'].update({ count: 1 })])
+  }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button
+  on:click={() => transact([tx.counter['singleton'].update({ count: count + 1 })])}
+>{count}</button>
